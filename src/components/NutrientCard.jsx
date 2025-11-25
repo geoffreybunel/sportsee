04ff58c262931029b793ccd2c3049e53a6e1bdd0
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-11-25 16:50:45 +0100

    fixed css height nutrient card

--- src/components/NutrientCard.jsx
+++ src/components/NutrientCard.jsx
@@ -1,6 +1,6 @@
 function NutrientCard({icon, name, color, value}) {
     return (
-        <div className="bg-[#FBFBFB] h-31 flex items-center gap-3.5 p-5 h-auto">
+        <div className="bg-[#FBFBFB] flex items-center gap-3.5 p-5 h-auto">
             <div className="w-15 h-15 flex justify-center items-center rounded-md" style={{ backgroundColor: `${color}33` }}>
                 <img src={`../../src//assets/${icon}-icon.png`} alt={`${icon} icon`} />
             </div>
